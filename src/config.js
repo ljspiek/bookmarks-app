472efc704882a78cb6cd731afe482e53a5517fac
Laura Spieker <ljspiek@gmail.com>
2020-01-10 20:14:29 -0600

    first time deploying to production

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$/LrHekmuCwXYhLPBOSk77.UZ3wmgcSWI76IMSf17z6ljES3mSGptm',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
